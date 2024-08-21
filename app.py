7cfa6b61de6dcacc02b620550d424e5e8439e338
douglasmatosdev <douglasmatosdev@gmail.com>
2024-08-21 08:27:17 -0300

    render chart without dropdown

--- app.py
+++ app.py
@@ -3,48 +3,30 @@ import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 
-dataset = pd.read_excel("./datasource/eletrecidade.xlsx")
+data_eletrecidade = pd.read_excel("./datasource/eletrecidade.xlsx")
 
 app = Dash(__name__)
 
 server = app.server
 
-app.layout = html.Div([
-    html.H1('Projeto de extensão'),
-    html.P("Selecione o mês:"),
-    dcc.Dropdown(
-        id="dropdown",
-        options=dataset["Referência"].unique(),
-        value='Gold',
-        clearable=False,
-    ),
-    dcc.Graph(id="graph"),
-])
 
-
-@app.callback(
-    Output("graph", "figure"), 
-    Input("dropdown", "value"))
-def display_color(month):
-    fig = px.bar(
-    dataset,
+fig = px.bar(
+    data_eletrecidade,
     x="Referência",
     y="Valor",
     title="Gastos com Eletrecidade de 2021 à 2024",
     labels={"Valor": "Valor (R$)", "Referência": "Meses/Ano"},
 )
-    return fig
 
 
-app.run_server(debug=False)
+app.layout = html.Div([
+    html.H1('Projeto de extensão'),
+    dcc.Graph(
+        id="graph",
+        figure=fig
+    ),
+])
 
 
-# fig = px.bar(
-#     dataset,
-#     x="Referência",
-#     y="Valor",
-#     title="Gastos com Eletrecidade de 2021 à 2024",
-#     labels={"Valor": "Valor (R$)", "Referência": "Meses/Ano"},
-# )
 
-# fig.show()
+app.run_server(debug=True)
