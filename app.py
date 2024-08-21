4ef45bc6dc8c5e1829406934194d84af2293b1e4
douglasmatosdev <douglasmatosdev@gmail.com>
2024-08-21 09:08:39 -0300

    gastos com internet

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
-from dash import Dash, dcc, html, Input, Output
-import plotly.graph_objects as go
+from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
 
-data_eletrecidade = pd.read_excel("./datasource/eletrecidade.xlsx")
+data_eletricidade = pd.read_excel("./datasource/eletricidade.xlsx")
+data_internet= pd.read_excel("./datasource/internet.xlsx")
 
 app = Dash(__name__)
 
 server = app.server
 
-fig = px.bar(
-    data_eletrecidade,
+figEletricidade = px.bar(
+    data_eletricidade,
     x="Referência",
     y="Valor",
-    title="Gastos com Eletrecidade de 2021 à 2024",
+    title="Gastos com Eletricidade de 2021 à 2024",
     labels={"Valor": "Valor (R$)", "Referência": "Meses/Ano"},
+)
+
+figInternet = px.bar(
+    data_internet,
+    x="PERÍODO",
+    y="VALOR",
+    title="Gastos com Internet de 2022 à 2024",
+    labels={"VALOR": "Valor (R$)", "PERÍODO": "Meses/Ano"},
 )
 
 app.layout = html.Div([
     html.H1('Projeto de extensão'),
     dcc.Graph(
-        id="graph",
-        figure=fig
+        id="graph_eletricidade",
+        figure=figEletricidade
     ),
+    dcc.Graph(
+        id="graph_internet",
+        figure=figInternet
+    )
 ])
 
 app.run_server(debug=True, port=8050, host='0.0.0.0')
